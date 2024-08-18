d1e79971bdcd7f7614cd1658298405882d1c65ab
Ayush Tamang <tamangayush052@gmail.com>
2024-08-18 14:31:14 +0545

    made bar-chart

--- Client/src/Admin/Admin.jsx
+++ Client/src/Admin/Admin.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import Header from './components/Header.jsx';
-import SideBar from './components/SideBar.jsx';
-import Content from './components/Content.jsx';
+import React from "react";
+import Header from "./components/Header.jsx";
+import SideBar from "./components/SideBar.jsx";
+import Content from "./components/Content.jsx";
+import Chart from "./components/Chart.jsx";
 
 function Admin() {
   return (
@@ -9,8 +10,13 @@ function Admin() {
       <Header />
       <div className="flex">
         <SideBar />
-        <div className='w-full'>
-        <Content />
+        <div className="w-full overflow-auto">
+          <Content />
+          <div className="p-4">
+            <div className="w-full max-w-full overflow-x-auto">
+              <Chart />
+            </div>
+          </div>
         </div>
       </div>
     </>
