d1bcbe2ae439c26d5454e455535f4430b90146c8
Ayush Tamang <tamangayush052@gmail.com>
2024-08-29 22:41:12 +0545

    organize imports and export components for admin dashboard

--- Client/src/Admin/components/SideBar.jsx
+++ Client/src/Admin/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TbLayoutDashboard } from "react-icons/tb";
 import { LuUser2 } from "react-icons/lu";
 import { LuFlagTriangleRight } from "react-icons/lu";
@@ -9,18 +9,17 @@ import { CiDiscount1 } from "react-icons/ci";
 function SideBar() {
   return (
     <nav className="bg-primary p-[20px] h-[89.5vh]">
-        
       <div className="dashboard flex items-center gap-[.5rem] m-2">
         <Link to="/admin">
-        <TbLayoutDashboard className="icon" />
-        <span className="nav-name">Dashboard</span>
+          <TbLayoutDashboard className="icon" />
+          <span className="nav-name">Dashboard</span>
         </Link>
       </div>
 
       <div className="dashboard flex items-center gap-[.5rem] m-2">
         <Link to="/user">
-        <LuUser2 className="icon" />
-        <span className="nav-name">All User</span>
+          <LuUser2 className="icon" />
+          <span className="nav-name">All User</span>
         </Link>
       </div>
 
@@ -36,21 +35,22 @@ function SideBar() {
 
       <div className="dashboard flex items-center gap-[.5rem] m-2">
         <Link to="/transaction">
-        <AiOutlineDollar className="icon" />
-        <span className="nav-name">Transactions</span>
+          <AiOutlineDollar className="icon" />
+          <span className="nav-name">Transactions</span>
         </Link>
       </div>
 
       <div className="dashboard flex items-center gap-[.5rem] m-2">
-        <CiDiscount1 className="icon" />
-        <span className="nav-name">Discount & Offer</span>
+        <Link to="/discount">
+          <CiDiscount1 className="icon" />
+          <span className="nav-name">Discount & Offer</span>
+        </Link>
       </div>
 
       <div className="dashboard flex items-center gap-[.5rem] m-2">
         <AiOutlineDollar className="icon" />
         <span className="nav-name">Sales History</span>
       </div>
-
     </nav>
   );
 }
---
+++ Client/src/App.js
@@ -0,0 +1,28 @@
+import LoginPage from "./Pages/LoginPage/LoginPage.jsx";
+import SignUp from "./Pages/SignUp/SignUp.jsx";
+import Admin from "./Admin/Admin.jsx"
+import Visual from "./Admin/components/Content.jsx";
+import Chart from "./Admin/components/Chart/Chart.jsx";
+import User from "./Admin/pages/User.jsx";
+import Vendor from "./Admin/pages/Vendor.jsx";
+import Staff from "./Admin/pages/Staff.jsx";
+import DeliveryBoy from "./Admin/pages/DeliveryBoy.jsx"
+import Transaction from "./Admin/pages/Transaction.jsx";
+import FourButton from "./Admin/components/Button/FourButton.jsx";
+import Discount from "./Admin/pages/Discount.jsx";
+
+
+export{
+    LoginPage,
+    SignUp,
+    Admin,
+    Visual,
+    Chart,
+    User,
+    Vendor,
+    Staff,
+    DeliveryBoy,
+    Transaction,
+    FourButton,
+    Discount
+}
