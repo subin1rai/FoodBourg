0a47eda5e3362cc871521ac0d50ac4aefbe04069
Ayush Tamang <tamangayush052@gmail.com>
2024-08-31 23:11:06 +0545

    done basic setup for backend

---
+++ Server/routes/route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+// router.post("/register", registerUser)
+// router.get("/read", readUser)
+// router.put("/update/:id", updateUser)
+// router.delete("/delete/:email", deleteUser)
+// router.post("/login/:email", loginStudent)
